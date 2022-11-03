f0823190c3150294a666e6f10e6de39cb8f3c28d
Ali <anix1@kent.edu>
2022-11-02 19:59:04 -0600

    updated seed data

--- seeds/01_emotions.js
+++ seeds/01_emotions.js
@@ -2,17 +2,15 @@
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> } 
  */
-exports.seed = async function(knex) {
-  // Deletes ALL existing entries
-  await knex('emotionsTable').del()
-  await knex('emotionsTable').insert(
-    // {"greeting": "hey"}
+ exports.seed = async function(knex) {
+  await knex('emotions').del()
+  await knex('emotions').insert(
   [
-    {id: 1, emotionType: 'anger'},
-    {id: 2, emotionType: 'stress'},
-    {id: 3, emotionType: 'cool'},
-    {id: 4, emotionType: 'swaggilicious'},
-    {id: 5, emotionType: 'happy'}
+    {id: 1, name: 'anger'},
+    {id: 2, name: 'stress'},
+    {id: 3, name: 'fear'},
+    {id: 4, name: 'worry'},
+    {id: 5, name: 'doubt'}
   ]
   );
 };
