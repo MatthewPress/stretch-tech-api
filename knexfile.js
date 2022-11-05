a43a5d32d33d5e965fdd25ba1c9c75bd179c6bf8
Anthony Shellman <atshellman@gmail.com>
2022-11-05 11:36:49 -0600

    Use varable for prod connection string

--- knexfile.js
+++ knexfile.js
@@ -1,5 +1,5 @@
 // Update with your config settings.
-
+const DATABASE_URL = process.env.DATABASE_URL
 /**
  * @type { Object.<string, import("knex").Knex.Config> }
  */
@@ -19,7 +19,7 @@
   production: {
     client: "postgresql",
     connection: {
-      connectionString: "postgres://lodrgnncesajuj:e637fad108a6d3db25c4ede24082908373183c18fdff03743f71bc4ca7fdf3c0@ec2-34-194-40-194.compute-1.amazonaws.com:5432/d2sdtff13mk8lf", 
+      connectionString: DATABASE_URL, 
       ssl: {
         rejectUnauthorized: false,
       },
